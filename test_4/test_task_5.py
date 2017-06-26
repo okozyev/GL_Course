580eb08a9f5a69acb684af3a6aa7f63595be3734
Oleksiy Kozyev <oleksiy.kozyev@globallogic.com>
2017-06-27 00:10:29 +0300

    task 5 (done)

--- test_4/test_task_5.py
+++ test_4/test_task_5.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 
 
-
 def test_task_5():
+
+#   Select Browser (del '#')
+
     driver = webdriver.Chrome()
     # driver = webdriver.Firefox()
     # driver = webdriver.Ie()
@@ -15,17 +17,29 @@ def test_task_5():
     # seek_prod_title = driver.find_element_by_css_selector('a[title="Yellow Duck"]').click()
 
 #   taking values from tab
-#     seek_prod_name = driver.find_element_by_css_selector('a[title="Yellow Duck"]').text
-    seek_prod_name = driver.find_element_by_xpath("//div[@class='name']")
+    seek_prod_name = driver.find_element_by_xpath("//div[@class='name']").text
+
     seek_prod_reg_price = driver.find_element_by_css_selector('s[class="regular-price"]').text
     seek_prod_reg_price_decor = driver.find_element_by_css_selector('s[class="regular-price"]').value_of_css_property('text-decoration-line')
     seek_prod_reg_price_color = driver.find_element_by_css_selector('s[class="regular-price"]').value_of_css_property('color')
 
+    seek_prod_low_price = driver.find_element_by_css_selector('strong[class="campaign-price"]').text
+    seek_prod_low_price_decor = driver.find_element_by_css_selector('strong[class="campaign-price"]').value_of_css_property('font-weight')
+    seek_prod_low_price_color = driver.find_element_by_css_selector('strong[class="campaign-price"]').value_of_css_property('color')
+
+#   click to open
     seek_prod_title = driver.find_element_by_css_selector('a[title="Yellow Duck"]').click()
 
 #   taking values from opened
     prod_name = driver.find_element_by_xpath("//h1[@class='title']").text
+
     prod_reg_price = driver.find_element_by_xpath("//del[@class='regular-price']").text
+    prod_reg_price_decor = driver.find_element_by_xpath("//del[@class='regular-price']").value_of_css_property('text-decoration-line')
+    prod_reg_price_color = driver.find_element_by_xpath("//del[@class='regular-price']").value_of_css_property('color')
+
+    prod_low_price = driver.find_element_by_xpath("//strong[@class='campaign-price']").text
+    prod_low_price_decor = driver.find_element_by_xpath("//strong[@class='campaign-price']").value_of_css_property('font-weight')
+    prod_low_price_color = driver.find_element_by_xpath("//strong[@class='campaign-price']").value_of_css_property('color')
 
 #    comparement
     print('Product Name is equal on Main page and Item Page - ', seek_prod_name == prod_name)
@@ -34,11 +48,27 @@ def test_task_5():
     print('Product RegPrice is equal on Main page and Item Page - ', seek_prod_reg_price == prod_reg_price)
     assert (seek_prod_reg_price == prod_reg_price), "Product Price doesn't match"
 
+    print('Product RegPrice decor (Line) is equal on Main page and Item Page - ', seek_prod_reg_price_decor == prod_reg_price_decor)
+    assert (seek_prod_reg_price_decor == prod_reg_price_decor), "Product RegPrice decor (Line) doesn't match"
 
-#
+    print('Product RegPrice color is equal on Main page and Item Page - ', seek_prod_reg_price_color == prod_reg_price_color)
+    assert (seek_prod_reg_price_color == prod_reg_price_color), "Product RegPrice color doesn't match"
+
+    print('Product Campaign Price is equal on Main page and Item Page - ', seek_prod_low_price == prod_low_price)
+    assert (seek_prod_low_price == prod_low_price), "Product Campaign Price doesn't match"
+
+    print('Product Campaign Price font weight is equal on Main page and Item Page - ', seek_prod_low_price_decor == prod_low_price_decor)
+    assert (seek_prod_low_price_decor == prod_low_price_decor), "Product Campaign Price font weight doesn't match"
+
+    print('Product Campaign Price font color is equal on Main page and Item Page - ', seek_prod_low_price_color == prod_low_price_color)
+    assert (seek_prod_low_price_color == prod_low_price_color), "Product Campaign Price color doesn't match"
+
+#   end of process
+    driver.quit()
+
 
 # xpath seek =   //*[@id="box-campaign-products"]/div/div/div/a/div[2]/div[1]  <div class="name">Yellow Duck</div>
 # xpath prod =   //*[@id="box-product"]/div[1]/div[2]/h1   <h1 class="title">Yellow Duck</h1>
 
-    # seek_prod_name = driver.find_element_by_xpath("//div[@class='name']")
-    # prod_name = driver.find_element_by_xpath("//h1[@class='title']")
+# seek_prod_name = driver.find_element_by_xpath("//div[@class='name']")
+# prod_name = driver.find_element_by_xpath("//h1[@class='title']")
